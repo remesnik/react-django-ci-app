50b7563fe79d6f70c5a0003f54d299b2df5dbe68
Brute <adam.remesnik@gmail.com>
2025-06-18 16:45:35 -0700

    .gitignore update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from dotenv import load_dotenv
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -75,10 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    ),
+    'testing': dj_database_url.config(
+        default=os.getenv('TEST_DATABASE_URL')
+    )
 }
 
 
